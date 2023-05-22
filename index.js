c8b09bfe1b461f6bb75cc20f89e6137aee834c2f
Hemanth J <53926166+hemanthkrish@users.noreply.github.com>
2023-05-22 18:35:34 +0530

    Update index.js
    some new changes

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-// Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-//const appDiv = document.getElementById('app');
-//appDiv.innerHTML = ``;
-
-
-document.getElementById('submit').onclick = function(){
+document.getElementById('submit').onclick = function () {
   let tot = document.getElementById('tot_classes').value;
   let pre = document.getElementById('pre_classes').value;
   let glpc = document.getElementById('goal_percent').value;
 
-  let answ = (pre - ((glpc/100) * tot)) / ((glpc/100) - 1);
-  answ=Math.round(answ)
+  let answ = (pre - (glpc / 100) * tot) / (glpc / 100 - 1);
+  answ = Math.round(answ);
 
-  console.log(tot);
-  console.log(pre);
-  console.log(answ);
-
-document.getElementById('answer').innerHTML=answ+' classes';
-document.getElementById('answer').innerHTML=(answ/37)+' classes';
-  
+  document.getElementById('answer').innerHTML = answ + ' classes'+'🤟';
 };
-//const cal_att = () => {
- // ;
-//}
